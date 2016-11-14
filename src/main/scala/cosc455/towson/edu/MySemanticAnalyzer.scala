3477a4915c290eace0cd8565c9599cf595d667a8
malnea1 <malnea1@students.towson.edu>
2016-11-13 22:03:18 -0500

    3

--- src/main/scala/cosc455/towson/edu/MySemanticAnalyzer.scala
+++ src/main/scala/cosc455/towson/edu/MySemanticAnalyzer.scala
@@ -3,6 +3,44 @@ package cosc455.towson.edu
 /**
   * Created by MBH on 11/13/2016.
   */
-class MySemanticAnalyzer {
+class MySemanticAnalyzer extends SyntaxAnalyzer{
+  override def gittex(): Unit = {
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)){
+      // add to parse tree / stack
+      Compiler.Scanner.getNextToken()
+    }
+    else {
+      println("Error")
+      System.exit(1)
+    }
+  }
+
+  override def paragraph(): Unit = ???
+
+  override def inneritem(): Unit = ???
+
+  override def innertext(): Unit = ???
+
+  override def link(): Unit = ???
+
+  override def italics(): Unit = ???
+
+  override def body(): Unit = ???
+
+  override def bold(): Unit = ???
+
+  override def newline(): Unit = ???
+
+  override def title(): Unit = ???
+
+  override def vardef(): Unit = ???
+
+  override def images(): Unit = ???
+
+  override def varuse(): Unit = ???
+
+  override def heading(): Unit = ???
+
+  override def listitem(): Unit = ???
 
 }
